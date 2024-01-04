3985a44d9a4ee9ffc1add8f9d137398285fec362
martin1724 <martinmatyja1724@gmail.com>
2024-01-04 16:50:47 +0100

    final draft

--- bir_yt.py
+++ bir_yt.py
@@ -5,7 +5,7 @@
 # SPDX-License-Identifier: GPL-3.0
 #
 # GNU Radio Python Flow Graph
-# Title: Top Block
+# Title: Not titled yet
 # GNU Radio version: 3.10.8.0
 
 from PyQt5 import Qt
@@ -24,17 +24,17 @@ from gnuradio import eng_notation
 from gnuradio import soapy
 from gnuradio.qtgui import Range, RangeWidget
 from PyQt5 import QtCore
+import bir_yt_ook_demod_block as ook_demod_block  # embedded python block
 import sip
-import top_block_ook_demod_block as ook_demod_block  # embedded python block
 
 
 
-class top_block(gr.top_block, Qt.QWidget):
+class bir_yt(gr.top_block, Qt.QWidget):
 
     def __init__(self):
-        gr.top_block.__init__(self, "Top Block", catch_exceptions=True)
+        gr.top_block.__init__(self, "Not titled yet", catch_exceptions=True)
         Qt.QWidget.__init__(self)
-        self.setWindowTitle("Top Block")
+        self.setWindowTitle("Not titled yet")
         qtgui.util.check_set_qss()
         try:
             self.setWindowIcon(Qt.QIcon.fromTheme('gnuradio-grc'))
@@ -52,7 +52,7 @@ class top_block(gr.top_block, Qt.QWidget):
         self.top_grid_layout = Qt.QGridLayout()
         self.top_layout.addLayout(self.top_grid_layout)
 
-        self.settings = Qt.QSettings("GNU Radio", "top_block")
+        self.settings = Qt.QSettings("GNU Radio", "bir_yt")
 
         try:
             geometry = self.settings.value("geometry")
@@ -65,11 +65,17 @@ class top_block(gr.top_block, Qt.QWidget):
         # Variables
         ##################################################
         self.channel_freq = channel_freq = 433920000
-        self.samp_rate = samp_rate = 2400000
+        self.samp_rate_0 = samp_rate_0 = 2400000
+        self.samp_rate = samp_rate = 32000
+        self.number_of_preamble_bits = number_of_preamble_bits = 6
         self.manchester_decoding = manchester_decoding = True
         self.fftsize = fftsize = 512
+        self.edge_offset = edge_offset = 20
         self.channel_width = channel_width = 20000
         self.center_freq = center_freq = channel_freq
+        self.Inter = Inter = 1
+        self.Decim = Decim = 2
+        self.Dead_space = Dead_space = 5000
 
         ##################################################
         # Blocks
@@ -103,22 +109,27 @@ class top_block(gr.top_block, Qt.QWidget):
 
         self._soapy_rtlsdr_source_0_setting_keys = [a.key for a in self.soapy_rtlsdr_source_0.get_setting_info()]
 
-        self.soapy_rtlsdr_source_0.set_sample_rate(0, samp_rate)
+        self.soapy_rtlsdr_source_0.set_sample_rate(0, 3200000)
         self.soapy_rtlsdr_source_0.set_frequency(0, center_freq)
         self.soapy_rtlsdr_source_0.set_frequency_correction(0, 0)
         self.set_soapy_rtlsdr_source_0_bias(bool(False))
         self._soapy_rtlsdr_source_0_gain_value = 20
         self.set_soapy_rtlsdr_source_0_gain_mode(0, bool(False))
         self.set_soapy_rtlsdr_source_0_gain(0, 'TUNER', 20)
+        self.rational_resampler_xxx_0 = filter.rational_resampler_fff(
+                interpolation=Inter,
+                decimation=Decim,
+                taps=[],
+                fractional_bw=0)
         self.qtgui_time_sink_x_0_1_0 = qtgui.time_sink_f(
-            fftsize, #size
-            samp_rate, #samp_rate
-            "Magnitude", #name
-            2, #number of inputs
+            150000, #size
+            1600000, #samp_rate
+            "Threshold", #name
+            1, #number of inputs
             None # parent
         )
         self.qtgui_time_sink_x_0_1_0.set_update_time(0.10)
-        self.qtgui_time_sink_x_0_1_0.set_y_axis(0, 0.4)
+        self.qtgui_time_sink_x_0_1_0.set_y_axis(-1.0, 1.0)
 
         self.qtgui_time_sink_x_0_1_0.set_y_label('Amplitude', "")
 
@@ -145,7 +156,7 @@ class top_block(gr.top_block, Qt.QWidget):
             -1, -1, -1, -1, -1]
 
 
-        for i in range(2):
+        for i in range(1):
             if len(labels[i]) == 0:
                 self.qtgui_time_sink_x_0_1_0.set_line_label(i, "Data {0}".format(i))
             else:
@@ -158,40 +169,32 @@ class top_block(gr.top_block, Qt.QWidget):
 
         self._qtgui_time_sink_x_0_1_0_win = sip.wrapinstance(self.qtgui_time_sink_x_0_1_0.qwidget(), Qt.QWidget)
         self.top_layout.addWidget(self._qtgui_time_sink_x_0_1_0_win)
-        self.ook_demod_block = ook_demod_block.blk(symbol_rate=4e3, sample_rate=1e6, manchester_decoding=True)
+        self.ook_demod_block = ook_demod_block.blk(preamble_bits=number_of_preamble_bits, edge_offset=edge_offset, dead_space=Dead_space)
+        self.interp_fir_filter_xxx_0 = filter.interp_fir_filter_fff(1, [1,1])
+        self.interp_fir_filter_xxx_0.declare_sample_delay(0)
         self._channel_width_range = Range(500, 50000, 100, 20000, 200)
         self._channel_width_win = RangeWidget(self._channel_width_range, self.set_channel_width, "'channel_width'", "counter_slider", float, QtCore.Qt.Horizontal)
         self.top_layout.addWidget(self._channel_width_win)
         self._channel_freq_range = Range(433.92e6, 433.92e6, 1000, 433920000, 200)
         self._channel_freq_win = RangeWidget(self._channel_freq_range, self.set_channel_freq, "'channel_freq'", "counter_slider", float, QtCore.Qt.Horizontal)
         self.top_layout.addWidget(self._channel_freq_win)
-        self.blocks_threshold_ff_0 = blocks.threshold_ff(0.04, 0.06, 0)
+        self.blocks_threshold_ff_0 = blocks.threshold_ff(0.3, 0.5, 0)
         self.blocks_complex_to_mag_0 = blocks.complex_to_mag(1)
-        self.band_pass_filter_0_0 = filter.fir_filter_ccc(
-            1,
-            firdes.complex_band_pass(
-                1,
-                samp_rate,
-                (-10000),
-                10000,
-                10e3,
-                window.WIN_HAMMING,
-                6.76))
 
 
         ##################################################
         # Connections
         ##################################################
-        self.connect((self.band_pass_filter_0_0, 0), (self.blocks_complex_to_mag_0, 0))
-        self.connect((self.blocks_complex_to_mag_0, 0), (self.blocks_threshold_ff_0, 0))
-        self.connect((self.blocks_complex_to_mag_0, 0), (self.qtgui_time_sink_x_0_1_0, 0))
+        self.connect((self.blocks_complex_to_mag_0, 0), (self.interp_fir_filter_xxx_0, 0))
         self.connect((self.blocks_threshold_ff_0, 0), (self.ook_demod_block, 0))
-        self.connect((self.blocks_threshold_ff_0, 0), (self.qtgui_time_sink_x_0_1_0, 1))
-        self.connect((self.soapy_rtlsdr_source_0, 0), (self.band_pass_filter_0_0, 0))
+        self.connect((self.blocks_threshold_ff_0, 0), (self.qtgui_time_sink_x_0_1_0, 0))
+        self.connect((self.interp_fir_filter_xxx_0, 0), (self.rational_resampler_xxx_0, 0))
+        self.connect((self.rational_resampler_xxx_0, 0), (self.blocks_threshold_ff_0, 0))
+        self.connect((self.soapy_rtlsdr_source_0, 0), (self.blocks_complex_to_mag_0, 0))
 
 
     def closeEvent(self, event):
-        self.settings = Qt.QSettings("GNU Radio", "top_block")
+        self.settings = Qt.QSettings("GNU Radio", "bir_yt")
         self.settings.setValue("geometry", self.saveGeometry())
         self.stop()
         self.wait()
@@ -205,14 +208,24 @@ class top_block(gr.top_block, Qt.QWidget):
         self.channel_freq = channel_freq
         self.set_center_freq(self.channel_freq)
 
+    def get_samp_rate_0(self):
+        return self.samp_rate_0
+
+    def set_samp_rate_0(self, samp_rate_0):
+        self.samp_rate_0 = samp_rate_0
+
     def get_samp_rate(self):
         return self.samp_rate
 
     def set_samp_rate(self, samp_rate):
         self.samp_rate = samp_rate
-        self.band_pass_filter_0_0.set_taps(firdes.complex_band_pass(1, self.samp_rate, (-10000), 10000, 10e3, window.WIN_HAMMING, 6.76))
-        self.qtgui_time_sink_x_0_1_0.set_samp_rate(self.samp_rate)
-        self.soapy_rtlsdr_source_0.set_sample_rate(0, self.samp_rate)
+
+    def get_number_of_preamble_bits(self):
+        return self.number_of_preamble_bits
+
+    def set_number_of_preamble_bits(self, number_of_preamble_bits):
+        self.number_of_preamble_bits = number_of_preamble_bits
+        self.ook_demod_block.preamble_bits = self.number_of_preamble_bits
 
     def get_manchester_decoding(self):
         return self.manchester_decoding
@@ -225,6 +238,13 @@ class top_block(gr.top_block, Qt.QWidget):
 
     def set_fftsize(self, fftsize):
         self.fftsize = fftsize
+
+    def get_edge_offset(self):
+        return self.edge_offset
+
+    def set_edge_offset(self, edge_offset):
+        self.edge_offset = edge_offset
+        self.ook_demod_block.edge_offset = self.edge_offset
 
     def get_channel_width(self):
         return self.channel_width
@@ -239,10 +259,29 @@ class top_block(gr.top_block, Qt.QWidget):
         self.center_freq = center_freq
         self.soapy_rtlsdr_source_0.set_frequency(0, self.center_freq)
 
+    def get_Inter(self):
+        return self.Inter
+
+    def set_Inter(self, Inter):
+        self.Inter = Inter
+
+    def get_Decim(self):
+        return self.Decim
+
+    def set_Decim(self, Decim):
+        self.Decim = Decim
+
+    def get_Dead_space(self):
+        return self.Dead_space
+
+    def set_Dead_space(self, Dead_space):
+        self.Dead_space = Dead_space
+        self.ook_demod_block.dead_space = self.Dead_space
 
 
 
-def main(top_block_cls=top_block, options=None):
+
+def main(top_block_cls=bir_yt, options=None):
 
     qapp = Qt.QApplication(sys.argv)
 
---
+++ decode.py
@@ -0,0 +1,295 @@
+"""
+Embedded Python Blocks:
+
+Each time this file is saved, GRC will instantiate the first class it finds
+to get ports and parameters of your block. The arguments to __init__  will
+be the parameters. All of them are required to have default values!
+"""
+
+import numpy as np
+from gnuradio import gr
+import sys
+
+data_set = np.array([])
+start = int(0)
+stop = int(0)
+size = int(0)
+state = int(1)
+keep_track_flag = int(0)
+old_message = []
+
+class blk(gr.sync_block):  # other base classes are basic_block, decim_block, interp_block
+    """Embedded Python Block example - a simple multiply const"""
+    
+
+    def __init__(self, preamble_bits=1, edge_offset=1, dead_space=1):  # only default arguments here
+        """arguments to this function show up as parameters in GRC"""
+        gr.sync_block.__init__(
+            self,
+            name='Custom Data Decoder',   # will show up in GRC
+            in_sig=[np.float32],
+            out_sig=None
+        )
+        # if an attribute with the same name as a parameter is found,
+        # a callback is registered (properties work, too).
+        self.preamble_bits = preamble_bits
+        self.edge_offset = edge_offset
+        self.dead_space = dead_space
+
+    def work(self, input_items, output_items):
+        """example: multiply with constant"""
+        in0 = np.array(input_items[0])
+        
+        global state
+        global data_set
+        global start
+        global stop
+        global size
+        global keep_track_flag
+        global old_message
+        
+        if state == 1:
+            #Looking for a blank space before the message.
+            if np.any(in0 > 0.5):
+                #Reset size and start over. 
+                size = 0
+            else:
+                size = size + len(in0)
+                #Once enough blank space has gone by start looking for the message.
+                if size > self.dead_space: #This value is imperically found. 
+                    size = 0
+                    state = 2
+        
+        #look for a leading edge
+        if state == 2:
+            #Calculate sample 1 minus sample 2. This looks for any edge positive or negative. 
+            leading_edge = np.abs(in0[:-1] - in0[1:]) > .5
+            #Check if any edge is found. 
+            if np.any(leading_edge == True):
+                #When edge is found start looking for the trailing edge
+                state = 3
+                #Store the data because it will span several frames.
+                data_set = np.append(data_set, in0)
+                
+            
+            
+        #look for a trailing edge
+        if state == 3:
+            #Keep storing the data while looking for the trailing edge. 
+            data_set = np.append( data_set, in0)
+            #Calculate sample 1 minus sample 2. This looks for any edge positive or negative.
+            trailing_edge = np.abs(in0[:-1] - in0[1:]) > 0.5
+            #Check if any go positive. 
+            if np.any(trailing_edge == True):
+                size = 0
+            else:
+                size = size + len(in0)
+                #if a long enough stretch is found after the message the it ended. 
+                if size > self.dead_space: #This value is imperically found.
+                    size = 0
+                    state = 4
+            
+            
+        #analyze the data
+        if state == 4:
+            
+            
+            #file = open('C:\logs\data_set.txt','a')
+            #intermediate = data_set.astype(int)
+            #intermediate2 = ",\r".join(intermediate.astype(str))
+            #file.write(intermediate2)
+            #file.write("\r,space\r,")
+            #file.close()
+            
+            
+            #If the data set is greater than 70000 the it is the first packet
+            #The first packet has a wake up signal of 72 bits and then the message.
+            if len(data_set) > 30000:
+                #Find the edges
+                edges = np.abs(data_set[:-1] - data_set[1:]) > 0.5
+                #Grab the locations of the edges.
+                edge_locations = np.where(edges)[0]
+                #Find the difference between the edges.
+                edge_difference = edge_locations[1:] - edge_locations[:-1]
+                #debug
+                #print 'data length', len(data_set)
+                #print 'edge diferences', edge_difference[:20]
+                #Grab the edge difference into the packet preamble.
+                try:
+                    #Sometime this picks up garbadge and the try helps to keep it from crashing
+                    average = edge_difference[self.preamble_bits * 2]
+                    average_found = True
+                except:
+                    average_found = False
+                #Make sure that there are enough edges to do something usefull. 
+                if average_found and len(edge_difference) > (self.preamble_bits * 2):
+                    #Trim down how big the edge difference is.
+                    edge_difference = edge_difference[:self.preamble_bits * 2]
+                    #print 'average', average
+                    #Filter edge_difference to eliminate everything greater than 110% and less than 90%
+                    #This should eliminate everything at the begining that is not the preable. 
+                    looking_for_start_edge_positive = edge_difference < (average * 1.1)
+                    looking_for_start_edge_negative = edge_difference > (average * 0.9)
+                    #Logical and together the two to get something that eliminates both. 
+                    looking_for_start_edge = np.logical_and(looking_for_start_edge_positive, looking_for_start_edge_negative)
+                    #print looking_for_start_edge[:15]
+                    #Grab all the locations that are true. 
+                    check_if_consecutive = np.where(looking_for_start_edge)[0]
+                    #print 'check if consecutive', check_if_consecutive
+                    #print 'len of check if consecutive', len( check_if_consecutive)
+                    #Create an array that should be the same if it is consecutive. 
+                    consecutive_array = np.arange(check_if_consecutive[0], check_if_consecutive[-1] + 1)
+                    #print 'consecutive array', consecutive_array
+                    #Ther arrays may be different sizes and causing the check to throw and exception.
+                    
+                    
+                    #Set compare to false ahead of time.
+                    compare = False
+                    for i in range(3):
+                        #If they are different sizes then they can't be consecuitve.
+                        if len( check_if_consecutive) == len( consecutive_array):
+                            #Compare together the consecutive array and the check if consecutive. 
+                            #If the arrays are not consecutive the check will fail. 
+                            if np.all(np.equal(check_if_consecutive, consecutive_array)):
+                                compare = True
+                                #If the arrays are consecutive leave the for loop.
+                                break
+                        #Sometimes just one stray edge is the correct size.
+                        #Try trimming the first edge and check for consecutive.
+                        #Because of the for loop this is done up to 3 times. 
+                        #If the array is not consecutive by then, it won't be. 
+                        check_if_consecutive = check_if_consecutive[1:]
+                        #Create an array that should be the same if it is consecutive. 
+                        consecutive_array = np.arange(check_if_consecutive[0], check_if_consecutive[-1] + 1)
+                        
+                    if compare:
+                        #Average together the preamble to get what the clock speed is. 
+                        period = int(np.average(edge_difference[check_if_consecutive[0]: check_if_consecutive[-1] + 1]))
+                        #debug
+                        #print 'check_if_consecutive', check_if_consecutive[0]
+                        #print 'edge locations', edge_locations[:10]
+                        #print 'the preable was consecustive', edge_locations[check_if_consecutive[0]]
+                        #This is where the leading edge of the preable is. 
+                        #It is offset by one to properly align the manchester coding.
+                        start = edge_locations[check_if_consecutive[0]]
+                        #Trim edge locations
+                        edge_locations = edge_locations[check_if_consecutive[0]:]
+                        
+                        
+                        #Now create a sampling array that alignes with the data. 
+                        ticks_positive = np.arange(start - self.edge_offset, (start - self.edge_offset) + (period * 88), period, dtype=np.int)
+                        ticks_negative = np.arange(start + self.edge_offset, (start - self.edge_offset) + (period * 88), period, dtype=np.int)
+                        
+                        # To keep things from crashing
+                        if ticks_negative[-1] < len(data_set):
+                            #The data seems to have some gitter or drift. 
+                            #Make an array of only desired edges.
+                            optimised_edges = np.array([edge_locations[0]])
+                            for g in range(1,len(edge_locations)):
+                                if((edge_locations[g]-optimised_edges[-1])<(period*1.2))and((edge_locations[g]-optimised_edges[-1])>(period*0.8)):
+                                    optimised_edges = np.append(optimised_edges,edge_locations[g])
+                            #Now that we have the edges we can optimise the sampling locations.
+                            for h in range(len(optimised_edges)):
+                                if ticks_positive[h] >= optimised_edges[h]:
+                                    adjustment = ticks_positive[h] - optimised_edges[h] + self.edge_offset
+                                    ticks_negative[h:] -= adjustment
+                                    ticks_positive[h:] -= adjustment
+                                    
+                                if ticks_negative[h] <= optimised_edges[h]:
+                                    adjustment = optimised_edges[h] - ticks_negative[h] + self.edge_offset
+                                    ticks_negative[h:] += adjustment
+                                    ticks_positive[h:] += adjustment
+                            
+                            # Convert the data to high and low 
+                            converted_data = data_set > 0.5
+                            #Reshape the data 
+                            positive_packet = converted_data[ticks_positive].reshape(88)
+                            neagtive_packet = converted_data[ticks_negative].reshape(88)
+                            
+                            # if keep_track_flag == 0:
+                                # file = open('C:\logs\data_set.txt','a')
+                                # intermediate = data_set.astype(int)
+                                # intermediate2 = ",\r".join(intermediate.astype(str))
+                                # file.write(intermediate2)
+                                # file.write(",\r space,\r")
+                                # intermediate =  ",\r".join(ticks_positive.astype(str))
+                                # file.write(intermediate)
+                                # file.write(",\r space,\r")
+                                # intermediate =  ",\r".join(ticks_negative.astype(str))
+                                # file.write(intermediate)
+                                # file.write(",\r space,\r")
+                                # file.close()
+                                # keep_track_flag = 1
+                            
+                            
+                            
+                            if np.all(np.logical_xor(positive_packet,neagtive_packet)):
+                                #Without knowing what the information is supposed to look like
+                                #it is a 50/50 chance as to using the positive packet or negative packet.
+                                #The difference being that one gives you IEEE manchester coding
+                                #and the other gives you Thomas coding. 
+                                packet = positive_packet.reshape(11,8)
+                                message = []
+                                message2 = np.zeros((11,), dtype=int)
+                                for j in range(11):
+                                    decimal = sum([packet[j,7-i]*2**i for i in range(len(packet[j,]))])
+                                    message2[j] = decimal
+                                    message.append(str("{0:#0{1}x}".format(decimal,4)))
+                                
+                                
+                                if old_message != message:
+                                    old_message = message
+                                    print message[2:]
+                                    message2[8] = message2[8] - 27
+                                    if message2[8] < 0:
+                                        message2[8] = 256 + message2[8]
+                                        
+                                    message2[7] += 1
+                                    if message2[7] > 255:
+                                        message2[7] = 0
+                                        message2[6] += 1
+                                        if message2[5] > 255:
+                                            message2[5] = 0
+                                            
+                                    message2[10] = np.sum(message2[2:10])
+                                    while message2[10] > 255:
+                                        message2[10] -= 256
+                                    message3 = []
+                                    for j in range(11):
+                                        message3.append(str("{0:#0{1}x}".format(message2[j],4)))
+                                        
+                                    print "The next message is"
+                                    print message3[2:]
+                                    
+                                    # concatinated_message = []
+                                    # for j in range(11):
+                                        # decimal = sum([packet[j,7-i]*2**i for i in range(len(packet[j,]))])
+                                        # concatinated_message.append(str("{0:02x}".format(decimal,2)))
+                                    
+                                    
+                                    # file = open('C:\logs\data_set.txt','a')
+                                    # intermediate = "".join(concatinated_message)
+                                    # file.write(intermediate)
+                                    # file.write(" ")
+                                    # file.close()
+                                    
+                            else:
+                                print 'The xor did not resolve.'
+                        else:
+                            print 'index out during reshape', len( data_set), ticks_negative[-1]
+                         
+                    else:
+                        print 'the preable was not consecustive', check_if_consecutive, len( check_if_consecutive)
+                else:
+                    print 'average failed'
+            else:
+                print 'the data set was too short'
+            #Reset all the variable for another go around
+            state = 1
+            size = 0
+            start = 0
+            stop = 0
+            data_set = np.array([])
+        
+        
+        return len(input_items[0])
